8db1dd46e21d7707ea0281eb8dc45a48281aa404
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-01-13 13:32:27 +0100

    Create volwelstoone.js

---
+++ volwelstoone.js
@@ -0,0 +1,16 @@
+// Name: Nathália Pimentel de Assis
+//University: University of Brasilia
+//Bachelor student in eletronic engineering course
+//Date: November 4th 2022
+
+
+function trocaVogais(string) {
+  let arr = "";
+  for (let i = 0; i < string.length; i++){
+    let char = string[i];
+    if(char === "a" ||char === "e" ||char=== "i" ||char=== "o" ||char=== "u"){
+      arr = arr +  "1";
+    } else {
+      arr += char;
+    }
+  }
